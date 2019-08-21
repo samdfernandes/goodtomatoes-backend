abd394de6b8f4ead05fd76776d2dfa13b765825b
Sammy <asfernandes@sbcglobal.net>
2019-08-21 11:38:54 -0400

    changed info on model

--- models/memberSchema.js
+++ models/memberSchema.js
@@ -3,12 +3,9 @@ const mongoose = require('mongoose')
 const memberSchema = mongoose.Schema({
     name: { type: String },
     email: { type: String },
-    bio: { type: String },
-    memberPic: { type: String },
-    medicalNotes: { type: String },
-    skills: [{ type: String }],
-    ableToWork: { type: Boolean },
-    needs: [{ type: String }]
+    friends: [{ type: String }],
+    likes: [{ type: String }],
+    reviews: [{ type: String }],
 })
 
 module.exports = mongoose.model('Member', memberSchema)

69832f644e8bfd57cb577c4ebf5e91a49718650c
Mushnik <cmorrison@Coreys-Mac.local>
2019-08-21 12:18:14 -0400

    added review Schema and updated server.js

--- models/donorSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const donorSchema = mongoose.Schema({
-    name: { type: String },
-    email: { type: String }
-})
-
-module.exports = mongoose.model('Donor', donorSchema)
--- server.js
+++ server.js
@@ -4,15 +4,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const membersController = require('./controllers/members');
-const donorsController = require('./controllers/donors');
 const PORT = 3003;
 const cors = require('cors');
 
 //cors
-const whitelist = [
-  'http://localhost:3000',
-  'https://fathomless-sierra-68956.herokuapp.com'
-];
+const whitelist = ['http://localhost:3000', 'http://good_tomatoes.surge.sh/'];
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1) {
@@ -28,7 +24,6 @@ app.use(cors());
 
 app.use(express.json());
 app.use('/members', membersController);
-app.use('/donors', donorsController);
 
 //mongoose connections
 mongoose.connection.on('error', err =>
@@ -45,24 +40,14 @@ mongoose.connection.once('open', () => {
 
 // SEED ROUTE ////////////////////////////////////
 const seed = require('./controllers/seedMember');
-const seed2 = require('./controllers/seedDonor');
 
 ///// Uncomment the line below this to seed the data from the seedMember file. Then go to /seedMembers. Comment out line when done.
-const memberSeed = require('./models/memberSchema');
-const donorSeed = require('./models/donorSchema');
+// const memberSeed = require('./models/memberSchema');
 
 //seeds the members
 app.get('/seedMembers', (req, res) => {
   memberSeed.create(seed, (err, createdMembers) => {
     console.log(createdMembers);
-    res.redirect('/');
-  });
-});
-
-//seeds the donors
-app.get('/seedDonors', (req, res) => {
-  donorSeed.create(seed, (err, createdDonors) => {
-    console.log(createdDonors);
     res.redirect('/');
   });
 });

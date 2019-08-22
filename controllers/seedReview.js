7b66bd00f3ff32dcbb42a5f698accd4ca737e526
David Carrano <davidcarrano@Macs-MacBook-Pro.local>
2019-08-22 11:58:50 -0400

    updated seedReview

--- controllers/seedReview.js
+++ controllers/seedReview.js
@@ -1,9 +1,15 @@
 let seedReviews = [
     {
-        createdBy: '',
-        title: '',
-        rating: null,
-        reviewNotes: '',
+        createdBy: 'David Carrano',
+        title: 'Jaws',
+        rating: 4,
+        reviewNotes: 'Classic! Timeless'
+    },
+    {
+        createdBy: 'David Carrano',
+        title: 'The Witch',
+        rating: 4,
+        reviewNotes: 'Very realistic New England setting. Disturbing'
     }
 ];
 
